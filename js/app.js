b4a120c6af5f64d76ed2e7fc723312a450f30aba
Yousuke Tezuka <YousukeTezuka@gmail.com>
2014-11-07 00:36:30 +0900

    Finish #3

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 (function() {
 
+  // Model
+
+  var Task = Backbone.Model.extend({
+    defaults: {
+      title: "do someting!",
+      completed: false
+    }
+  });
+
+  var task1 = new Task({
+    title: "do it!",
+    completed: true
+  });
+  console.log(task1.toJSON());
+
+  var task2 = new Task();
+  console.log(task2.toJSON());
+
 })();
