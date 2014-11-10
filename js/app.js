a4357b54590ff7aec28a1db777fa72671a3cb07d
Yousuke Tezuka <YousukeTezuka@gmail.com>
2014-11-10 14:05:45 +0900

    Finish #13

--- js/app.js
+++ js/app.js
@@ -1,21 +1,34 @@
 (function() {
 
-  var Task = Backbone.Model.extend({});
+  var Task = Backbone.Model.extend({
+    defaults: {
+      title: 'do samething!',
+      completed: false
+    }
+  });
   var Tasks = Backbone.Collection.extend({model: Task});
 
-  var TaskView = Backbone.View.extend({});
+  var TaskView = Backbone.View.extend({
+    tagName: 'li',
+    template: _.template($('#task-template').html()),
+    render: function(){
+      var template = this.template(this.model.toJSON());
+      this.$el.html(template);
+      return this;
+    }
+  });
   var TasksView = Backbone.View.extend({});
 
   var tasks = new Tasks([
     {
-      title: "task1",
+      title: 'task1',
       completed: true
     },
     {
-      title: "task2"
+      title: 'task2'
     },
     {
-      title: "task3"
+      title: 'task3'
     }
   ]);
 
